f21eff5d98d311b2f2df39fae045e389cc95667c
elsawanjiru <elsa.wanjiru@student.moringaschool.com>
2023-08-18 21:41:11 +0300

    Completed Assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
+def return_evens(sequence):
+    even_elements = [num for num in sequence if num % 2 == 0]
+    return even_elements if even_elements else []
 
 def make_exclamation(sentence_list):
-    pass
+    if not sentence_list:
+        return []
+    exclaimed_sentences = [sentence + '!' for sentence in sentence_list]
+    return exclaimed_sentences
